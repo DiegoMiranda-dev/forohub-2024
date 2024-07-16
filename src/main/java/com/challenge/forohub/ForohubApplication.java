0d21373d69d9975e520bc98777ec38f75cc51a84
DiegoMiranda-dev <mirandadiego864@gmail.com>
2024-07-16 14:28:08 -0600

    executable

--- src/main/java/com/challenge/forohub/ForohubApplication.java
+++ src/main/java/com/challenge/forohub/ForohubApplication.java
@@ -1,7 +1,5 @@
 package com.challenge.forohub;
 
-import com.challenge.forohub.service.TopicoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
