3350ebf78b7396bd80ab9dcddb69e4714483c055
DiegoMiranda-dev <mirandadiego864@gmail.com>
2024-07-16 02:10:27 -0600

    executable

--- src/main/java/com/challenge/forohub/ForohubApplication.java
+++ src/main/java/com/challenge/forohub/ForohubApplication.java
@@ -1,13 +1,16 @@
 package com.challenge.forohub;
 
+import com.challenge.forohub.service.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ForohubApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ForohubApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForohubApplication.class, args);
+    }
 
 }
