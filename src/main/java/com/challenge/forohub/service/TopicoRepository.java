f7da3f62c83acf5ec15d2707ab89534db7faa5b0
DiegoMiranda-dev <mirandadiego864@gmail.com>
2024-07-16 02:12:32 -0600

    main interface

---
+++ src/main/java/com/challenge/forohub/service/TopicoRepository.java
@@ -0,0 +1,12 @@
+package com.challenge.forohub.service;
+
+import com.challenge.forohub.topico.Topico;
+import io.micrometer.observation.ObservationFilter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+    Page<Topico> findByActivoTrue(Pageable paginacion);
+
+}
